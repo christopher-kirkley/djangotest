5520e9ad4277142bd3ff026019fcf16ac66b1fcf
Christopher Kirkley <ckirkley@gmail.com>
2022-03-29 19:35:26 -0700

    initial rest api build

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from api.serializers import UserSerializer
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from api.serializers import UserSerializer, GroupSerializer
+
+
 def index(request):
     return HttpResponse("hey")
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
